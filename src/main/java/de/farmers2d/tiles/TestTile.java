df6d698570d1b81654c03bb01678fbb05c0d58da
fen4flo <fb8599422@gmail.com>
2025-05-12 09:05:42 +0200

    Update TestTile.java

--- src/main/java/de/farmers2d/tiles/TestTile.java
+++ src/main/java/de/farmers2d/tiles/TestTile.java
@@ -2,12 +2,14 @@ package de.farmers2d.tiles;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import de.demoncore.utils.Vector3;
 import de.farmers2d.tiles.core.Tile;
 import de.farmers2d.tiles.core.Tileset;
 
-public class TestTile extends Tile {
+public class TestTile extends Tile {	
 
 	public TestTile(Tileset tileset) {
 		super(tileset);
@@ -16,9 +18,9 @@ public class TestTile extends Tile {
 	@Override
 	public void draw(Graphics2D g2d, Vector3 screenPos, Vector3 size) {
 		g2d.setColor(new Color(
-				(byte)(getTileset().getRandom().nextFloat() * 25),
-				(byte)(getTileset().getRandom().nextFloat() * 25),
-				(byte)(getTileset().getRandom().nextFloat() * 25),
+				(byte)(getTileset().getRandom().nextFloat() * 100),
+				(byte)(getTileset().getRandom().nextFloat() * 100),
+				(byte)(getTileset().getRandom().nextFloat() * 100),
 				255));
 		
 		g2d.fillRect((int)screenPos.x, (int)screenPos.y, (int)size.x, (int)size.y);
